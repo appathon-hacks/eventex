af7bb87aeb82b0d5fc41c2fc94197fb5e8a297d3
Abhijeet <void.aby@gmail.com>
2015-02-08 08:42:33 +0530

    init

---
+++ app/src/main/java/voidabhi/com/chatbubbleexample/ChatMessage.java
@@ -0,0 +1,15 @@
+package voidabhi.com.chatbubbleexample;
+
+/**
+ * Created by ABHIJEET on 06-02-2015.
+ */
+public class ChatMessage {
+    public boolean left;
+    public String message;
+
+    public ChatMessage(boolean left, String message) {
+        super();
+        this.left = left;
+        this.message = message;
+    }
+}
